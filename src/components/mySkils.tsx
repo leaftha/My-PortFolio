8f1e939f7bc8d39505ad6c10531bac38aa24f328
SaChan <vltpcks@gmail.com>
2024-09-09 21:06:02 +0900

    기술 스택 추가중

--- src/components/mySkils.tsx
+++ src/components/mySkils.tsx
@@ -1,7 +1,12 @@
 function mySkils() {
   return (
     <div>
-      <h1>My Skils</h1>
+      <div>
+        <p>동서대학교 재학중(2020년 입학 ~ 2026년 졸업예정)</p>
+      </div>
+      <div>
+        <h1>My Skils</h1>
+      </div>
     </div>
   );
 }

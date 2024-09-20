49319afd3d36906db934c160c84e34d9a2f32958
SaChan <vltpcks@gmail.com>
2024-09-20 18:42:08 +0900

    form구조 생성

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,7 +3,25 @@ import style from "../style/contact.module.css";
 function Contact() {
   return (
     <div className={style.main}>
-      <h1>Contact Me</h1>
+      <div className={style.titleContainer}>
+        <h1 className={style.title}>Contact Me</h1>
+      </div>
+
+      <form className={style.forms}>
+        <div className={style.formContent}>
+          <label className={style.formTitle}>이름</label>
+          <input className={style.inputs} />
+        </div>
+        <div className={style.formContent}>
+          <label className={style.formTitle}>연락처</label>
+          <input className={style.inputs} />
+        </div>
+        <label className={style.formTitle}>내용</label>
+        <textarea className={style.text} />
+        <div className={style.btnContainer}>
+          <button className={style.btn}>전송</button>
+        </div>
+      </form>
     </div>
   );
 }

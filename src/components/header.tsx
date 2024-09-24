1e3973c178ffd51923ed8c4ed8adb35aaeeb80a8
SaChan <vltpcks@gmail.com>
2024-09-24 20:29:17 +0900

    애니 효과 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,20 +5,20 @@ import { IsClickContext } from "./isClickedContext";
 import { useContext, useEffect, useState } from "react";
 import { UpRolling, slideUp } from "../util/ani";
 
-interface dimensions {
-  width: number;
-  height: number;
-}
+// interface dimensions {
+//   width: number;
+//   height: number;
+// }
 
 function Header() {
-  const [dimension, setDimension] = useState<dimensions>({
-    width: 0,
-    height: 0,
-  });
+  // const [dimension, setDimension] = useState<dimensions>({
+  //   width: 0,
+  //   height: 0,
+  // });
   const clicked = useContext(IsClickContext);
 
   useEffect(() => {
-    setDimension({ width: window.innerWidth, height: window.innerHeight });
+    // setDimension({ width: window.innerWidth, height: window.innerHeight });
 
     const lockScroll = () => {
       document.body.style.overflow = "hidden";
@@ -40,15 +40,15 @@ function Header() {
     };
   }, [clicked?.isClick]);
 
-  const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
-    dimension.height
-  } Q${dimension.width / 2} ${dimension.height + 300} 0 ${
-    dimension.height
-  }  L0 0`;
+  // const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
+  //   dimension.height
+  // } Q${dimension.width / 2} ${dimension.height + 300} 0 ${
+  //   dimension.height
+  // }  L0 0`;
 
-  const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
-    dimension.height
-  } Q${dimension.width / 2} ${dimension.height} 0 ${dimension.height}  L0 0`;
+  // const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
+  //   dimension.height
+  // } Q${dimension.width / 2} ${dimension.height} 0 ${dimension.height}  L0 0`;
 
   const str: string[] = [
     "M",
@@ -64,16 +64,16 @@ function Header() {
     "I",
     "O",
   ];
-  const curve = {
-    initial: {
-      d: initialPath,
-      transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1] },
-    },
-    animate: {
-      d: targetPath,
-      transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3 },
-    },
-  };
+  // const curve = {
+  //   initial: {
+  //     d: initialPath,
+  //     transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1] },
+  //   },
+  //   animate: {
+  //     d: targetPath,
+  //     transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3 },
+  //   },
+  // };
   return (
     <motion.div
       className={style.headerDiv}
--- src/util/ani.tsx
+++ src/util/ani.tsx
@@ -11,47 +11,13 @@ interface HeaderSlideAni {
 
 export const UpRolling = {
   first: {
-    scale: 10,
-    // opacity: 0,
+    opacity: "0",
   },
   animation: (i: number) => ({
-    // opacity: 1,
-    // y: 55,
-    scale: 1,
-    transition: { duration: 0.2, delay: 0.2 + 0.1 * i },
+    opacity: "1",
+    transition: { type: "spring", duration: 0.2, delay: 0.2 + 0.1 * i },
   }),
 };
-
-// export const UpRolling2 = {
-//   first: {
-//     opacity: 1,
-//   },
-//   animation: (i: number) => ({
-//     opacity: 0,
-//     y: 55,
-//     transition: { duration: 0.2, delay: 0.2 + 0.1 * i },
-//   }),
-// };
-
-// export const DownRolling: HeaderAni = {
-//   first: {
-//     opacity: 1,
-//   },
-//   animation: {
-//     opacity: 0,
-//     y: -45,
-//   },
-// };
-
-// export const DownRolling2: HeaderAni = {
-//   first: {
-//     opacity: 0,
-//   },
-//   animation: {
-//     opacity: 1,
-//     y: -45,
-//   },
-// };
 
 export const slideUp: HeaderSlideAni = {
   initial: {

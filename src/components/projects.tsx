0eab57b44caefa3b2587371bb494b346fc8f6d13
SaChan <vltpcks@gmail.com>
2024-10-12 17:59:15 +0900

    스크롤 프로젝트 방식 변경

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import style from "../style/Projects.module.css";
-import { motion, useTransform, useScroll, useInView } from "framer-motion";
+import { motion, useTransform, useScroll } from "framer-motion";
 import { projects } from "../util/project";
 
 const scaleAnimation = {
@@ -16,7 +16,7 @@ const scaleAnimation = {
     scale: 0,
     x: "-50%",
     y: "-50%",
-    closed: { duration: 0.4, ease: [0.32, 0, 0.67, 0] },
+    transition: { duration: 0.4, ease: [0.32, 0, 0.67, 0] },
   },
 };
 
@@ -24,10 +24,26 @@ function Projects() {
   const cursor = useRef<HTMLDivElement | null>(null);
   const cursorLabel = useRef<HTMLDivElement | null>(null);
   const projectBody = useRef<HTMLDivElement | null>(null);
-  // const isVeiw = useInView(projectBody);
   const [active, setActive] = useState(false);
   const { scrollYProgress } = useScroll({ target: projectBody });
-  const x = useTransform(scrollYProgress, [0, 1], ["10%", "-82%"]);
+
+  // MotionValue을 일반 숫자로 변환하기 위한 상태 변수
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const index = useTransform(
+    scrollYProgress,
+    [0, 0.2, 0.6, 0.8, 1],
+    [0, 1, 2, 3, 4]
+  );
+
+  useEffect(() => {
+    // index 값이 변경될 때마다 currentIndex 상태 업데이트
+    const unsubscribe = index.onChange((latest) => {
+      setCurrentIndex(Math.round(latest)); // 필요한 경우 소수점 반올림
+    });
+
+    return () => unsubscribe(); // 컴포넌트 언마운트 시 구독 해제
+  }, [index]);
 
   useEffect(() => {
     const moveCursorX = gsap.quickTo(cursor.current, "left", {
@@ -38,25 +54,29 @@ function Projects() {
       duration: 0.5,
       ease: "power3",
     });
-    const moveCursorLablerX = gsap.quickTo(cursorLabel.current, "left", {
+    const moveCursorLabelX = gsap.quickTo(cursorLabel.current, "left", {
       duration: 0.45,
       ease: "power3",
     });
-    const moveCursorLablerY = gsap.quickTo(cursorLabel.current, "top", {
+    const moveCursorLabelY = gsap.quickTo(cursorLabel.current, "top", {
       duration: 0.45,
       ease: "power3",
     });
 
-    window.addEventListener("mousemove", (e) => {
-      if (!cursor.current) return;
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!cursor.current || !cursorLabel.current) return;
       const { clientX, clientY } = e;
-      // const cursorWidth = cursor.current.offsetWidth / 2;
-      // const cursorHeight = cursor.current.offsetHeight / 2;
       moveCursorX(clientX);
       moveCursorY(clientY);
-      moveCursorLablerX(clientX);
-      moveCursorLablerY(clientY);
-    });
+      moveCursorLabelX(clientX);
+      moveCursorLabelY(clientY);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
   }, []);
 
   return (
@@ -64,75 +84,74 @@ function Projects() {
       <div className={style.main} ref={projectBody}>
         <h1 className={style.title}>My Projects</h1>
         <div className={style.Projects}>
-          <motion.div className={style.projectsbody} style={{ x }}>
-            {projects.map((project, idx) => (
-              <div key={idx} className={style.projectContainer}>
-                <div className={style.contents}>
-                  <motion.h1
-                    className={`${style.projectTitle} ${
-                      project.img.length !== 0 && style.under
-                    }`}
-                    onHoverStart={() => {
-                      project.img.length !== 0 && setActive(true);
-                    }}
-                    onHoverEnd={() => {
-                      project.img.length !== 0 && setActive(false);
-                    }}
-                    onClick={() => {
-                      project.img.length !== 0 && window.open(project.address);
-                    }}
-                  >
-                    {project.name}
-                  </motion.h1>
-                  <p
-                    className={style.git}
-                    onClick={() => {
-                      window.open(project.git);
-                    }}
-                  >
-                    {project.git}
-                  </p>
-                  <p>
-                    <span className={style.projectDescriptionTitle}>기간:</span>
-                    {project.time}
-                  </p>
-                  <p>
-                    <span className={style.projectDescriptionTitle}>역활:</span>
-                    {project.description.role}
-                  </p>
-                  <p>
-                    <span className={style.projectDescriptionTitle}>
-                      사용 기술:
-                    </span>
-                    {project.description.tech}
-                  </p>
-                  <p>
-                    <span className={style.projectDescriptionTitle}>
-                      프로젝트 설명:
-                    </span>
-                    {project.description.projectDescription}
-                  </p>
-                  <p>
-                    <span className={style.projectDescriptionTitle}>
-                      구체적인 기술적 도전:
-                    </span>
-                    {project.description.challenge}
-                  </p>
-                  <p>
-                    <span className={style.projectDescriptionTitle}>
-                      해결 과정:
-                    </span>
-                    {project.description.solution}
-                  </p>
-                  <p>
-                    <span className={style.projectDescriptionTitle}>소감:</span>
-                    {project.description.feedback}
-                  </p>
-                </div>
+          <div className={style.projectsbody}>
+            <div className={style.projectContainer}>
+              <div className={style.contents}>
+                <motion.h1
+                  className={`${style.projectTitle} ${
+                    projects[currentIndex]?.img.length !== 0 && style.under
+                  }`}
+                  onHoverStart={() => {
+                    projects[currentIndex]?.img.length !== 0 && setActive(true);
+                  }}
+                  onHoverEnd={() => {
+                    projects[currentIndex]?.img.length !== 0 &&
+                      setActive(false);
+                  }}
+                  onClick={() => {
+                    projects[currentIndex]?.img.length !== 0 &&
+                      window.open(projects[currentIndex].address);
+                  }}
+                >
+                  {projects[currentIndex]?.name}
+                </motion.h1>
+                <p
+                  className={style.git}
+                  onClick={() => {
+                    window.open(projects[currentIndex]?.git);
+                  }}
+                >
+                  {projects[currentIndex]?.git}
+                </p>
+                <p>
+                  <span className={style.projectDescriptionTitle}>기간:</span>
+                  {projects[currentIndex]?.time}
+                </p>
+                <p>
+                  <span className={style.projectDescriptionTitle}>역활:</span>
+                  {projects[currentIndex]?.description.role}
+                </p>
+                <p>
+                  <span className={style.projectDescriptionTitle}>
+                    사용 기술:
+                  </span>
+                  {projects[currentIndex]?.description.tech}
+                </p>
+                <p>
+                  <span className={style.projectDescriptionTitle}>
+                    프로젝트 설명:
+                  </span>
+                  {projects[currentIndex]?.description.projectDescription}
+                </p>
+                <p>
+                  <span className={style.projectDescriptionTitle}>
+                    구체적인 기술적 도전:
+                  </span>
+                  {projects[currentIndex]?.description.challenge}
+                </p>
+                <p>
+                  <span className={style.projectDescriptionTitle}>
+                    해결 과정:
+                  </span>
+                  {projects[currentIndex]?.description.solution}
+                </p>
+                <p>
+                  <span className={style.projectDescriptionTitle}>소감:</span>
+                  {projects[currentIndex]?.description.feedback}
+                </p>
               </div>
-            ))}
-            ;
-          </motion.div>
+            </div>
+          </div>
         </div>
       </div>
       <motion.div

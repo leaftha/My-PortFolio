7a86446e60be1a10c54c76a0da841fe18879c399
SaChan <vltpcks@gmail.com>
2024-10-26 20:26:56 +0900

    이미지 슬라이드 배치 완료

--- src/components/projectsStart.tsx
+++ src/components/projectsStart.tsx
@@ -1,5 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+import style from "../style/projectsStart.module.css";
+
 function ProjectsStart() {
-  return <div></div>;
+  const [width, setWidth] = useState<number>(0);
+
+  useEffect(() => {
+    const update = () => {
+      const width = window.innerWidth;
+      setWidth(width);
+    };
+    update();
+    window.addEventListener("resize", update);
+
+    return () => window.addEventListener("resize", update);
+  }, []);
+  return (
+    <div className={style.main}>
+      <div className={style.imgContianer}>
+        <img
+          className={style.img}
+          style={{ width: width }}
+          src={`${process.env.PUBLIC_URL}/image/diet-record.jpg`}
+          alt="프로젝트 이미지 1"
+        />
+        <img
+          className={style.img}
+          style={{ width: width }}
+          src={`${process.env.PUBLIC_URL}/image/reading-history.jpg`}
+          alt="프로젝트 이미지 2"
+        />
+        <img
+          className={style.img}
+          style={{ width: width }}
+          src={`${process.env.PUBLIC_URL}/image/my-travel.jpg`}
+          alt="프로젝트 이미지 3"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsStart;

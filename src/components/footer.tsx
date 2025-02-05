391687c1f8c633c424658754e05711843f84a94b
SaChan <vltpcks@gmail.com>
2025-02-05 21:08:04 +0900

    Sticky Footer 고치기

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,10 @@ import style from "../style/Footer.module.css";
 function Footer() {
   return (
     <div className={style.footerContainer}>
-      <div className={style.contactWrapper}>
-        <Contact />
+      <div className={style.footerWrapper}>
+        <div className={style.footerContent}>
+          <Contact />
+        </div>
       </div>
     </div>
   );
